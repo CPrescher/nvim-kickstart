f7cfba2e1c52f5f22088fae37805c1e9252085d3
Clemens Prescher <clemens.prescher@gmail.com>
2024-05-29 03:22:47 +0200

    set transparent background

--- init.lua
+++ init.lua
@@ -721,15 +721,6 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like
-      vim.cmd.hi 'Comment gui=none'
-    end,
   },
 
   -- Highlight todo, notes, etc in comments
@@ -866,4 +857,17 @@ require('null-ls').setup {
   },
 }
 
--- setup nvimtree
+-- setting up tokyonight to be transparent
+require('tokyonight').setup {
+  transparent = true,
+}
+vim.cmd.colorscheme 'tokyonight-night'
+
+--  let NvimTree respect the transparency
+vim.cmd [[hi NvimTreeNormal guibg=NONE ctermbg=NONE]]
+vim.cmd [[hi NvimTreeNormalNC guibg=NONE ctermbg=NONE]]
+
+-- let Telescope respect the transparency
+vim.cmd [[hi TelescopeNormal guibg=NONE]]
+vim.cmd [[hi TelescopeBorder guibg=NONE]]
+vim.cmd [[hi TelescopePromptBorder guibg=NONE]]
